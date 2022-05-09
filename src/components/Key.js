d907dbf90750a887f9a63f4d8e83947fea690fd6
VokazakDalv <Vokazak.Dalv@gmail.com>
2022-05-09 10:11:21 +0400

    feat: add common requirements for task

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,20 +1,13 @@
 import Control from './Control.js';
+import { specialKeys } from '../data/key-array.js';
 
-class Key extends Control {
-  constructor(parentNode, up = '', down = '') {
-    super();
-    const span = new Control(parentNode, 'span');
-    span.node.dataset.key = down;
-    span.node.addEventListener('mousedown', () => {
-      span.node.addEventListener('mouseout', () => {
-        span.node.classList.remove('span-click');
-      });
-      span.node.classList.add('span-click');
-      if (span.node.dataset.key === 'Tab') {
-        // console.log('Tab');
-      }
-    });
-    span.node.addEventListener('mouseup', (event) => {
+class Key {
+  constructor(parentNode, up = '', down = '', textArea = '') {
+    this.currentTextArea = textArea;
+    this.span = new Control(parentNode, 'span');
+    this.span.node.dataset.key = down;
+    this.span.node.addEventListener('mousedown', this.mouseDownHandle.bind(this));
+    this.span.node.addEventListener('mouseup', (event) => {
       event.currentTarget.classList.remove('span-click');
     });
     if (down === 'Backspace'
@@ -22,19 +15,78 @@ class Key extends Control {
       || down === 'Shift'
       || down === 'CapsLock'
     ) {
-      span.node.classList.add('big');
+      this.span.node.classList.add('big');
     }
     if (down === 'Space') {
-      span.node.classList.add('space');
+      this.span.node.classList.add('space');
     }
     if (down === 'Crtl') {
-      span.node.classList.add('ctrl');
+      this.span.node.classList.add('ctrl');
     }
-    const iUp = new Control(span.node, 'i', 'up');
-    const iDown = new Control(span.node, 'i', 'down');
-    iUp.node.innerText = up;
-    iDown.node.innerText = (down !== 'Space') ? down : '';
+    this.iUp = new Control(this.span.node, 'i', 'up', up);
+    this.iDown = new Control(this.span.node, 'i', 'down', (down !== 'Space') ? down : '');
+  }
+
+  mouseDownHandle(event) {
+    event.preventDefault();
+    this.currentTextArea.node.focus();
+    // console.log(this.currentTextArea.node.selectionStart);
+    const start = this.currentTextArea.node.selectionStart;
+    const end = this.currentTextArea.node.selectionEnd;
+
+    if (specialKeys.indexOf(this.span.node.dataset.key) < 0) {
+      const cursorPosition = this.currentTextArea.node.selectionStart;
+      this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start)
+        + this.iDown.node.innerText + this.currentTextArea.node.value.substring(end);
+      this.currentTextArea.node.selectionEnd = cursorPosition + 1;
+    }
+    if (this.span.node.dataset.key === 'Space') {
+      const cursorPosition = this.currentTextArea.node.selectionStart;
+      this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
+      } ${this.currentTextArea.node.value.substring(end)}`;
+      this.currentTextArea.node.selectionEnd = cursorPosition + 1;
+    }
+
+    this.span.node.addEventListener('mouseout', () => {
+      this.span.node.classList.remove('span-click');
+    });
+    this.span.node.classList.add('span-click');
+
+    if (this.span.node.dataset.key === 'Backspace') {
+      this.backSpaceHandle(start, end);
+    }
+    if (this.span.node.dataset.key === 'Del') {
+      this.delHandle(start);
+    }
+    if (this.span.node.dataset.key === 'Tab') {
+      const cursorPosition = this.currentTextArea.node.selectionStart;
+      this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
+      }\t${this.currentTextArea.node.value.substring(end)}`;
+      this.currentTextArea.node.selectionEnd = cursorPosition + 1;
+    }
+    if (this.span.node.dataset.key === 'Enter') {
+      const cursorPosition = this.currentTextArea.node.selectionStart;
+      this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
+      }\n${this.currentTextArea.node.value.substring(end)}`;
+      this.currentTextArea.node.selectionEnd = cursorPosition + 1;
+    }
+    if (this.span.node.dataset.key === 'CapsLock') {
+      this.span.node.classList.toggle('span-active');
+    }
+  }
+
+  backSpaceHandle(start, end) {
+    const cursorPosition = this.currentTextArea.node.selectionStart;
+    this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start - 1)
+      + this.currentTextArea.node.value.substring(end);
+    this.currentTextArea.node.selectionEnd = cursorPosition - 1;
+  }
+
+  delHandle(start) {
+    const cursorPosition = this.currentTextArea.node.selectionStart;
+    this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start)
+      + this.currentTextArea.node.value.substring(start + 1);
+    this.currentTextArea.node.selectionEnd = cursorPosition;
   }
 }
-
 export default Key;
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,13 +1,36 @@
 import Key from './Key.js';
-import keys from '../data/key-array.js';
+import { keys } from '../data/key-array.js';
 import Control from './Control.js';
 
 class Keyboard {
   constructor() {
+    this.collection = [];
+    this.form = new Control(document.body, 'form');
+    this.textArea = new Control(this.form.node, 'textarea');
     this.container = new Control(document.body, 'div', 'container');
     keys.forEach((elem) => {
-      this.key = new Key(this.container.node, '', elem);
+      this.key = new Key(this.container.node, '', elem, this.textArea);
+      this.collection.push(this.key.iDown);
+      if (this.key.iDown.node.innerText === 'CapsLock') {
+        this.key.span.node.addEventListener('click', this.changeLetterCase.bind(this));
+      }
     });
+    localStorage.setItem('chizhikov-collection', this.collection);
+  }
+
+  changeLetterCase() {
+    console.log(this.collection[15].node.innerText);
+    if (this.collection[15].node.innerText === 'q') {
+      this.collection.forEach((elem) => {
+        const letterContainer = elem;
+        letterContainer.node.innerText = elem.node.innerText.toUpperCase();
+      });
+    } else {
+      this.collection.forEach((elem) => {
+        const letterContainer = elem;
+        letterContainer.node.innerText = elem.node.innerText.toLowerCase();
+      });
+    }
   }
 }
 
