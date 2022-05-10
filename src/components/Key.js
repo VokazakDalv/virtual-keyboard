335491af2f3b367483d531b7b210bb0a33026a8c
VokazakDalv <Vokazak.Dalv@gmail.com>
2022-05-10 22:37:35 +0400

    refactor: replace several methods to one switch case

--- src/components/Key.js
+++ src/components/Key.js
@@ -13,25 +13,24 @@ class Key {
     if (down === 'Backspace' || down === 'Enter' || down === 'Shift' || down === 'CapsLock') {
       this.span.node.classList.add('big');
     }
-    if (down === 'Space') {
+    if (down === ' ') {
       this.span.node.classList.add('space');
     }
     if (down === 'Crtl') {
       this.span.node.classList.add('ctrl');
     }
     this.iUp = new Control(this.span.node, 'i', 'up', up);
-    this.iDown = new Control(this.span.node, 'i', 'down', (down !== 'Space') ? down : '');
+    this.iDown = new Control(this.span.node, 'i', 'down', down);
   }
 
   mouseDownHandle(event) {
     event.preventDefault();
     this.currentTextArea.node.focus();
-    // console.log(this.currentTextArea.node.selectionStart);
     const start = this.currentTextArea.node.selectionStart;
     const end = this.currentTextArea.node.selectionEnd;
     this.keyPrint(start, end);
-    if (this.span.node.dataset.key === 'Space') {
-      this.spaceHandle(start, end);
+    if (this.span.node.dataset.key === ' ') {
+      this.specialKeysHandle(' ');
     }
     this.span.node.addEventListener('mouseout', () => {
       this.span.node.classList.remove('span-click');
@@ -39,66 +38,64 @@ class Key {
     this.span.node.classList.add('span-click');
 
     if (this.span.node.dataset.key === 'Backspace') {
-      this.backSpaceHandle(start, end);
+      this.specialKeysHandle('Backspace');
     }
     if (this.span.node.dataset.key === 'Delete') {
-      this.delHandle(start);
+      this.specialKeysHandle('Delete');
     }
     if (this.span.node.dataset.key === 'Tab') {
-      this.tabHandle(start, end);
+      this.specialKeysHandle('Tab');
     }
     if (this.span.node.dataset.key === 'Enter') {
-      this.enterHandle(start, end);
+      this.specialKeysHandle('Enter');
     }
     if (this.span.node.dataset.key === 'CapsLock') {
       this.span.node.classList.toggle('span-active');
     }
   }
 
-  keyPrint(start, end) {
+  keyPrint(start, end, node = this.iDown.node) {
     if (specialKeys.indexOf(this.span.node.dataset.key) < 0) {
       const cursorPosition = this.currentTextArea.node.selectionStart;
       this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start)
-        + this.iDown.node.innerText + this.currentTextArea.node.value.substring(end);
+        + node.innerText + this.currentTextArea.node.value.substring(end);
       this.currentTextArea.node.selectionEnd = cursorPosition + 1;
     }
   }
 
-  tabHandle() {
-    const start = this.currentTextArea.node.selectionStart;
-    const end = this.currentTextArea.node.selectionEnd;
+  specialKeysHandle(key) {
     const cursorPosition = this.currentTextArea.node.selectionStart;
-    this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
-    }\t${this.currentTextArea.node.value.substring(end)}`;
-    this.currentTextArea.node.selectionEnd = cursorPosition + 1;
-  }
-
-  enterHandle(start, end) {
-    const cursorPosition = this.currentTextArea.node.selectionStart;
-    this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
-    }\n${this.currentTextArea.node.value.substring(end)}`;
-    this.currentTextArea.node.selectionEnd = cursorPosition + 1;
-  }
-
-  spaceHandle(start, end) {
-    const cursorPosition = this.currentTextArea.node.selectionStart;
-    this.currentTextArea.node.value = `${this.currentTextArea.node.value.substring(0, start)
-    } ${this.currentTextArea.node.value.substring(end)}`;
-    this.currentTextArea.node.selectionEnd = cursorPosition + 1;
-  }
-
-  backSpaceHandle(start, end) {
-    const cursorPosition = this.currentTextArea.node.selectionStart;
-    this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start - 1)
-      + this.currentTextArea.node.value.substring(end);
-    this.currentTextArea.node.selectionEnd = cursorPosition - 1;
-  }
-
-  delHandle(start) {
-    const cursorPosition = this.currentTextArea.node.selectionStart;
-    this.currentTextArea.node.value = this.currentTextArea.node.value.substring(0, start)
-      + this.currentTextArea.node.value.substring(start + 1);
-    this.currentTextArea.node.selectionEnd = cursorPosition;
+    const [node] = [this.currentTextArea.node];
+    const start = node.selectionStart;
+    const end = node.selectionEnd;
+    switch (key) {
+      case ' ':
+        console.log(start);
+        console.log(end);
+        console.log(node.value.substring(0, start));
+        console.log(node.value.substring(end));
+        node.value = `${node.value.substring(0, start)} ${node.value.substring(start)}`;
+        node.selectionEnd = cursorPosition + 1;
+        break;
+      case 'Delete':
+        node.value = node.value.substring(0, start) + node.value.substring(start + 1);
+        node.selectionEnd = cursorPosition;
+        break;
+      case 'Backspace':
+        node.value = node.value.substring(0, start - 1) + node.value.substring(end);
+        node.selectionEnd = cursorPosition - 1;
+        break;
+      case 'Enter':
+        node.value = `${node.value.substring(0, start)}\n${node.value.substring(end)}`;
+        node.selectionEnd = cursorPosition + 1;
+        break;
+      case 'Tab':
+        node.value = `${node.value.substring(0, start)}\t${node.value.substring(end)}`;
+        node.selectionEnd = cursorPosition + 1;
+        break;
+      default:
+        console.log('Нет таких значений');
+    }
   }
 
   arrowHandle() {
